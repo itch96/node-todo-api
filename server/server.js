5a3f9e0be860c1b48d63fb1a8f8dcca08f45e2ee
Rahul Gupta <useritch96@gmail.com>
2017-06-30 17:18:45 +0530

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+})
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
