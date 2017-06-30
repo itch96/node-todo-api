e0519812818d6deb4e5a303d4cbafa950cbe7a95
Rahul Gupta <useritch96@gmail.com>
2017-06-30 16:30:53 +0530

    Added test scripts and db post api

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var port = process.env.PORT || 8080;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.status(200).send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+module.exports = {app}
